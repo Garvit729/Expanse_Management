e40e34e174c0039a2e59691ebbe3d86049b7cdd7
Garvitjain729 <20224066.gdscmnnit.24@gmail.com>
2024-04-12 20:53:47 +0530

    first commit

---
+++ controllers/transectionCtrl.js
@@ -0,0 +1,29 @@
+const transectionModel = require("../models/transectionModel");
+
+const getAllTransection = async (req, res) => {
+    try {
+      
+      const transections = await transectionModel.find({});
+      res.status(200).json(transections);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(erorr);
+    }
+  };
+const addTransection = async (req, res) => {
+    try {
+      // const newTransection = new transectionModel(req.body);
+      const newTransection = new transectionModel(req.body);
+      await newTransection.save();
+      res.status(201).send("Transection Created");
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  };
+
+  module.exports = {
+    getAllTransection,
+    addTransection
+  };
+  
